91ab630f7e120b56b0c36a488fbaf91af43ec335
Isha Sangani <isangani@college.harvard.edu>
2021-09-26 13:52:46 -0700

    created starter react app

---
+++ starter-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  //put all your data collecting code here
+  return (
+    //if you run `npm start` without changing this file,
+    //you should get a blank page :)
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
